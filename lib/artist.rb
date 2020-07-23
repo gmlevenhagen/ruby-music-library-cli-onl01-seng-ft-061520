cf2de5b83c1bb5f8a89ee446376ef08a21d50748
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-07-23 12:25:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,10 @@
-require 'pry'
-
 class Artist
-  attr_accessor :name, :song, :musiclibrarycontroller, :musicimporter
   extend Concerns::Findable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+
+  attr_accessor :name, :songs
+
   @@all = []
 
   def initialize(name)
@@ -14,48 +16,14 @@ class Artist
     @@all
   end
 
-  def self.destroy_all
-    @@all.clear
-  end
-
-  def save
-    @@all << self
-  end
-
-  def self.create(artist)
-    artist = self.new(artist)
-    artist.save
-    artist
-  end
-
-  def songs
-    @songs
-  end
-
   def add_song(song)
+    @songs << song unless songs.include?(song)
     if song.artist == nil
       song.artist = self
-    else
-      nil
     end
-    if @songs.include?(song)
-      nil
-    else
-      @songs << song
-    end
-    song
   end
 
   def genres
-    @new_array = []
-    @songs.each do |song|
-      if @new_array.include?(song.genre)
-        nil
-      else
-        @new_array << song.genre
-      end
-    end
-    @new_array
+    self.songs.collect{|song| song.genre}.uniq
   end
-
 end
