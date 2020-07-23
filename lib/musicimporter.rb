6d1a21f84de0cc0ee75683da12380c00627015c4
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-07-23 12:40:37 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,17 +1,17 @@
 class MusicImporter
-  attr_accessor :path
+  attr_accessor :path, :song, :artist, :genre, :musiclibrarycontroller
+  def initialize(path)
+    @path = path
+  end
 
-    def initialize(path)
-     @path = path
-   end
+  def files
+    Dir.entries(@path).select! {|entry| entry.end_with?(".mp3")}
+  end
 
-    def files
-     Dir.entries(path).select{ |file| file.end_with?(".mp3") }
-   end
-
-    def import
-     files.each { |file| Song.create_from_filename(file) }
-   end
+  def import
+    self.files.each do |filename|
+      Song.create_from_filename(filename)
+    end
   end
 
 end
