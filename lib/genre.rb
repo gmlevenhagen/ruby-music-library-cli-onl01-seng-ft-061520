f8f42d7cb8e093556335e51d1c7496448a69382f
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-07-23 12:40:42 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,45 +1,48 @@
 require 'pry'
 
 class Genre
+  attr_accessor :name, :musiclibrarycontroller, :musicimporter
   extend Concerns::Findable
-   attr_accessor :name, :songs
+  @@all = []
 
-    @@all = []
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-    def initialize(name)
-     @name = name
-     @songs = []
+  def self.all
+    @@all
+  end
 
-      save
-   end
+  def self.destroy_all
+    @@all.clear
+  end
 
-    def self.all
-     @@all
-   end
+  def save
+    @@all << self
+  end
 
-    def self.destroy_all
-     self.all.clear
-   end
+  def self.create(genre)
+    genre = self.new(genre)
+    genre.save
+    genre
+  end
 
-    def save
-     @@all << self
-   end
+  def songs
+    @songs
+  end
 
-    def self.create(string)
-     new_genre = Genre.new(string)
-   end
+  def artists
+    @new_array = []
+    @songs.each do |song|
+      if @new_array.include?(song.artist)
+        nil
+      else
+        @new_array << song.artist
+      end
+    end
+    @new_array
+  end
 
-    def add_song(song)
-     self.songs << song unless @songs.include?(song)
-     self.artist << song.artist unless @songs.include?(song)
-     song.genre = self if song.genre.nil?
-   end
-
-    def artists
-    artists =  self.songs.collect do |song|
-       song.artist
-     end
-     artists.uniq
-   end
 
 end
